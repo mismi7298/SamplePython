d7c248967ebf9ff8f02a576255b9cbcd77517a32
mismi7298 <37594532+mismi7298@users.noreply.github.com>
2025-12-17 08:07:05 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sample_python',
+    version='0.1.0',
+    description='A sample python project for eicar test',
+    author='Naresh Kumar',
+    packages=find_packages(),
+    # Add other metadata like license, install_requires, etc. as needed
+)
