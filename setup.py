81398ee5b555e72e8125e0ce44ddaf9d3e26dc65
Ubuntu <ubuntu@ip-172-31-27-196.ec2.internal>
2025-12-29 12:18:15 +0000

    added sample pacakge

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     version='0.1.0',
     description='A sample python project for eicar test',
     author='Naresh Kumar',
-    packages=find_packages(),
+    packages=["pkg"],
+    include_package_data=True,
+    package_data={
+        "pkg": ["hello.txt"],
+    },
     # Add other metadata like license, install_requires, etc. as needed
 )
